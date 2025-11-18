2d5a7b890e3bf5a1b945d8c29c26b1099210df21
flychicken <flychicken1991@gmail.com>
2025-11-17 16:09:59 -0800

    update home page

--- resume-frontend/src/constants/trustedCompanies.js
+++ resume-frontend/src/constants/trustedCompanies.js
@@ -5,6 +5,11 @@ import netflixLogo from '../assets/logos/netflix.png';
 import salesforceLogo from '../assets/logos/salesforce.png';
 import airbnbLogo from '../assets/logos/airbnb.png';
 import stripeLogo from '../assets/logos/stripe.png';
+import walmartLogo from '../assets/logos/walmart-official.png';
+import jpmorganLogo from '../assets/logos/jpmorgan-official.png';
+import cvsLogo from '../assets/logos/cvs-official.png';
+import deltaLogo from '../assets/logos/delta-official.png';
+import fedexLogo from '../assets/logos/fedex-official.png';
 
 const TRUSTED_COMPANIES = [
   { name: 'Google', logo: googleLogo },
@@ -14,6 +19,11 @@ const TRUSTED_COMPANIES = [
   { name: 'Salesforce', logo: salesforceLogo },
   { name: 'Airbnb', logo: airbnbLogo },
   { name: 'Stripe', logo: stripeLogo },
+  { name: 'Walmart', logo: walmartLogo },
+  { name: 'JPMorgan Chase', logo: jpmorganLogo },
+  { name: 'CVS Health', logo: cvsLogo },
+  { name: 'Delta Air Lines', logo: deltaLogo },
+  { name: 'FedEx', logo: fedexLogo, logoHeight: 46 },
 ];
 
 export default TRUSTED_COMPANIES;
